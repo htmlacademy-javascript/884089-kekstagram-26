ae86f4041c27039c26c68f86cca89ea687a16062
Keks <keks@htmlacademy.ru>
2022-06-07 22:06:14 +0300

    :heavy_check_mark: Сборка #2

--- 2/js/main.js
+++ 2/js/main.js
@@ -1,23 +1,19 @@
 // developer.mozilla.org
-// (Функция, возвращающая случайное целое число из переданного диапазона включительно).
+// Функция, возвращающая случайное целое число из переданного диапазона включительно.
 function getRandomIntInclusive(min, max) {
   if(min < 0){
-    // alert('Диапазон может быть только положительный, включая ноль');
-    return false;
+    throw new Error('Диапазон может быть только положительный, включая ноль');
   }
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
+
 getRandomIntInclusive(0, 100);
 
 // Функция для проверки максимальной длины строки.
-function getLeng(string, maxlength) {
-  if(string.length > maxlength){
-    // alert('Комментарий не проходит по длине');
-    return false;
-  }
-  // alert('Комментарий проходит по длине');
-  return true;
+function checkLength(string, maxlength) {
+  return string.length < maxlength;
 }
-getLeng('new string', 4);
+
+checkLength('new string', 4);
