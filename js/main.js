cb8c6c57268ca20b4371f1b1beb1e80d879b8aa6
AlexDrags <vera2052@mail.ru>
2022-06-11 14:51:25 +0300

    Создал генератор 25 обьектов, но числа повторяются

--- js/main.js
+++ js/main.js
@@ -1,15 +1,14 @@
 // developer.mozilla.org
 // Функция, возвращающая случайное целое число из переданного диапазона включительно.
-function getRandomIntInclusive(min, max) {
-  if(min < 0){
+function getRandomIntInclusive(a, b) {
+  if(a < 0){
     throw new Error('Диапазон может быть только положительный, включая ноль');
   }
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min + 1)) + min;
+  const lower = Math.ceil(Math.min(Math.abs(a), Math.abs(b)));
+  const upper = Math.floor(Math.max(Math.abs(a), Math.abs(b)));
+  const result = Math.random() * (upper - lower + 1) + lower;
+  return Math.floor(result);
 }
-
-getRandomIntInclusive(0, 100);
 
 // Функция для проверки максимальной длины строки.
 function checkLength(string, maxlength) {
@@ -17,3 +16,68 @@ function checkLength(string, maxlength) {
 }
 
 checkLength('new string', 4);
+
+// Число сгенерированных объектов:
+const COUNT_FOTO_CARDS = 25;
+
+const MESAGGES = [
+  'Всё отлично!',
+  'В целом всё неплохо. Но не всё.',
+  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
+  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
+];
+
+const AUTHORS = [
+  'Вася Пупкин',
+  'Дюша Метёлкин',
+  'Иван Василич',
+  'Товарищ Саахов',
+  'Шурик',
+  'Тапок Убийца'
+];
+
+// Ф-я получения индекса элемента:
+function getRandom (elements) {
+  return elements[getRandomIntInclusive(0, elements.length - 1)];
+}
+
+// Ф-я создания объекта фотокарточки
+function createCard() {
+  return {
+    id: getRandomIntInclusive(1, 25),
+    url: `photos/${  getRandomIntInclusive(1, 25)  }.jpg`,
+    description: 'Какое то фото..',
+    likes: getRandomIntInclusive(15, 200),
+    comments: [
+      {
+        id:getRandomIntInclusive(1, 100),
+        avatar: `img/avatar-${ getRandomIntInclusive(1, 6) }.svg`,
+        message: getRandom(MESAGGES),
+        name: getRandom(AUTHORS),
+      },
+      {
+        id:getRandomIntInclusive(1, 100),
+        avatar: `img/avatar-${ getRandomIntInclusive(1, 6) }.svg`,
+        message: getRandom(MESAGGES),
+        name: getRandom(AUTHORS),
+      },
+      {
+        id:getRandomIntInclusive(1, 100),
+        avatar: `img/avatar-${ getRandomIntInclusive(1, 6) }.svg`,
+        message: getRandom(MESAGGES),
+        name: getRandom(AUTHORS),
+      },
+      {
+        id:getRandomIntInclusive(1, 100),
+        avatar: `img/avatar-${ getRandomIntInclusive(1, 6) }.svg`,
+        message: getRandom(MESAGGES),
+        name: getRandom(AUTHORS),
+      },
+    ],
+  };
+}
+
+// eslint-disable-next-line no-unused-vars
+const listElemets = Array.from({length: COUNT_FOTO_CARDS}, createCard);
