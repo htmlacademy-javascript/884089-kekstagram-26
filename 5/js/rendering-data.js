076659da86cd88b40daec42e5f86e5c8af9fb168
Keks <keks@htmlacademy.ru>
2022-06-17 21:45:19 +0300

    :heavy_check_mark: Сборка #5

--- 5/js/rendering-data.js
+++ 5/js/rendering-data.js
@@ -5,11 +5,11 @@ const pictures = document.querySelector('.pictures');
 //Ф-ия генерации объектов фотокарточки из модуля mock-data.js:
 const valueMocks = mocks();
 //Ф-ия отрисовки фотокарточек на странице с использованием данных из ф-и генерации:
-valueMocks.forEach((valueMock)=>{
+valueMocks.forEach(({url,likes,comments})=>{
   const pictureCard = pictureTemplate.cloneNode(true);
-  pictureCard.querySelector('.picture__img').src = valueMock['url'];
-  pictureCard.querySelector('.picture__likes').textContent = valueMock['likes'];
-  pictureCard.querySelector('.picture__comments').textContent = valueMock['comments'].length;
+  pictureCard.querySelector('.picture__img').src = url;
+  pictureCard.querySelector('.picture__likes').textContent = likes;
+  pictureCard.querySelector('.picture__comments').textContent = comments.length;
   pictures.append(pictureCard);
 });
 
