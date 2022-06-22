fa8650033e56da862189195947fa3125afb5066f
Keks <keks@htmlacademy.ru>
2022-06-23 01:55:32 +0300

    :heavy_check_mark: Сборка #6

---
+++ 6/js/main.js
@@ -0,0 +1,7 @@
+// import {pictures} from './rendering-data.js';
+// import {bigPictureImg} from './rendering-data.js';
+import { mocks } from './mock-data.js';
+import { renderPictureCards,getPictureCardTemplate } from './rendering-data.js';
+renderPictureCards(mocks,getPictureCardTemplate);
+// window.console.log(renderPictureCards(mocks,getPictureCardTemplate));
+
