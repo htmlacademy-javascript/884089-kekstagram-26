394e4f19131084925ff1a24db7c0d0d2394388a6
AlexDrags <vera2052@mail.ru>
2022-06-27 21:56:44 +0300

    Рефакторинг кода  второй части задания отрисовки

--- js/main.js
+++ js/main.js
@@ -1,7 +1,5 @@
-// import {pictures} from './rendering-data.js';
-// import {bigPictureImg} from './rendering-data.js';
 import { mocks } from './mock-data.js';
-import { renderPictureCards } from './rendering-data.js';
-renderPictureCards(mocks);
+import {renderPictures} from './render.js';
+renderPictures(mocks);
 // window.console.log(renderPictureCards(mocks,getPictureCardTemplate));
 
