53bcd5c50a4d001adf7f16fd2c28532392f0f9b8
AlexDrags <vera2052@mail.ru>
2022-06-29 09:50:37 +0300

    Убрал все выводы в консоль и лишний код

--- js/render.js
+++ js/render.js
@@ -1,5 +1,5 @@
 import {mocks} from './mock-data.js';
-// Константы найденых элеметов разметки шndex.html:
+// Константы найденых элеметов разметки index.html:
 const pictureTemplate = document.querySelector('#picture').content.querySelector('.picture');
 const pictures = document.querySelector('.pictures');
 const bigPicture = document.querySelector('.big-picture');
@@ -14,7 +14,6 @@ const bigPictureCommentsLoader = bigPicture.querySelector('.comments-loader');
 // Комментарии к изображению:
 const bigPictureCommentsList = bigPicture.querySelector('.social__comments');
 
-
 // Функция, заполняющая шаблон фотокарточки данными:
 function getPictureCardTemplate(cardData) {
   const pictureCard = pictureTemplate.cloneNode(true);
@@ -25,19 +24,7 @@ function getPictureCardTemplate(cardData) {
   return pictureCard;
 }
 
-// Функция, создания элемента разметки через шаблонную строку заполненого данными:
-// 1 вариант function createElementComment (comments){
-//   const item = `<li class="social__comment">
-//     <img
-//     class="social__picture"
-//     src="${comments.avatar}"
-//     alt="${comments.name}"
-//     width="35" height="35">
-//     <p class="social__text">${comments.message}</p>
-//   </li>`;
-//   return item;
-// }
-// 2 вариант(еле сделал)
+// Ф-я создания комментария и его заполнения данными:
 function createElementComment ({avatar, name, message}){
   const item = `<li class="social__comment">
     <img
@@ -71,49 +58,39 @@ function showBigPicture(data) {
   bigPictureDescription.textContent = data.description;
   bigPictureCommentsCount.textContent = data.comments.length;
 
-  // console.log(data.comments);
   // Цикл отрисовки списка комментариев:
   for(let i = 0; i < data.comments.length; i++){
-    // для первого варианта:
-    // const temp = createElementComment(data.comments[i]);
-    // для второго варианта:
     const {avatar, name, message} = data.comments[i];
-    const temp = createElementComment({avatar, name, message});
-    // console.log(temp);
-    bigPictureCommentsList.insertAdjacentHTML('afterbegin', temp);
-    // Почему не работает так ?
-    // bigPictureCommentsList.appendChild(temp);
-    // bigPictureCommentsList.textContent(temp);
+    const newComment = createElementComment({avatar, name, message});
+    bigPictureCommentsList.insertAdjacentHTML('afterbegin', newComment);
   }
+  bigPictureClose.addEventListener('click', onCloseClick);
 }
 // Ф-я открытия большого изображения по клику на соотвествующее превью:
 function onPicturesClick(evt){
-  // console.log(evt.target.parentElement);
   const currentPicture = evt.target.parentElement;
   const pictureData = mocks.find((el)=>+currentPicture.id === +el.id);
-  // console.log(pictureData);
   showBigPicture(pictureData);
-  // console.log(mocks);
 }
 
 pictures.addEventListener('click', onPicturesClick);
 
-// Ф-я на закрытие большого фото по клику на крестик:
-function onCloseClick(){
-  document.querySelector('body').classList.toggle('modal-open');
-  bigPicture.classList.toggle('hidden');
-  bigPictureCommentCount.classList.toggle('hidden');
-  bigPictureCommentsLoader.classList.toggle('hidden');
-}
-// Ф-я на закрытие большого фото по клавише ESC:
-function onPicturesKeydown(evt) {
-  if(evt.keyCode === 27){
-    document.querySelector('.big-picture').classList.add('hidden');
-    document.querySelector('body').classList.remove('modal-open');
-    bigPictureCommentCount.classList.remove('hidden');
-    bigPictureCommentsLoader.classList.remove('hidden');
-  }
+// Ф-я на закрытие большого фото:
+function hiddenBigPicture(){
+  document.querySelector('.big-picture').classList.add('hidden');
+  document.querySelector('body').classList.remove('modal-open');
+  bigPictureCommentCount.classList.remove('hidden');
+  bigPictureCommentsLoader.classList.remove('hidden');
 }
 
+function onCloseClick(){
+  hiddenBigPicture();
+  bigPictureClose.removeEventListener('click', onCloseClick);
+}
+
+function onPicturesKeydown(evt) {
+  if(evt.keyCode === 27){
+    hiddenBigPicture();
+  }
+}
 pictures.addEventListener('keydown', onPicturesKeydown);
-bigPictureClose.addEventListener('click', onCloseClick);
