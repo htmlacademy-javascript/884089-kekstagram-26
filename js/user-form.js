943ac1a5b231c2303847c823c081da3514348f14
AlexDrags <vera2052@mail.ru>
2022-07-10 19:50:19 +0300

    Доработал проверки поля хэштега

--- js/user-form.js
+++ js/user-form.js
@@ -4,59 +4,78 @@ const fieldUpload = form.querySelector('#upload-file');
 const closeEditFormImg = form.querySelector('#upload-cancel');
 const imgUploadPreview = form.querySelector('.img-upload__preview');
 const fieldHashtag = form.querySelector('.text__hashtags');
-// const fieldDescription = form.querySelector('.text__description');
+const fieldDescription = form.querySelector('.text__description');
+
 const pristine = new Pristine(form,{
   classTo:'img-upload__field-wrapper',
   errorTextParent:'img-upload__field-wrapper',
   errorTextClass:'text__error'
 });
+
+const onFormSubmit = (evt)=>{
+  evt.preventDefault();
+  if(pristine.validate()){
+    form.submit();
+  }
+};
+
+form.addEventListener('submit', onFormSubmit);
+
+
+const textLengthComments = (value) => {
+  if( value.length <= 140 || !value.length){
+    return true;
+  }
+};
+
 pristine.addValidator(
-  form.querySelector('.text__description'),
-  (value)=> value.length >= 140,
+  fieldDescription,
+  textLengthComments,
   'длина комментария не может составлять больше 140 символов',
   1,
   true
 );
 
+const maxLengthHashtags = (value) => value.trim().split(' ').length < 5 ;
+
 pristine.addValidator(
-  form.querySelector('.text__hashtags'),
-  (value)=> value.length >= 2 && value.length <= 20,
-  'От 2 до 20 символов включая #',
+  fieldHashtag,
+  maxLengthHashtags,
+  'нельзя указать больше пяти хэш-тегов',
+  3,
+  true
+);
+
+const hashtagValidator = (value)=> {
+  const re = /^#[A-Za-zА-Яа-яЁё0-9]{1,19}$/;
+  if(!value.length){
+    return true;
+  }
+  return value.trim().split(' ').every((element) => re.test(element));
+};
+
+pristine.addValidator(
+  fieldHashtag,
+  hashtagValidator,
+  'хэш-тег начинается с символа #,от 2 до 20 символов включая #, и не может содержать пробелы, спецсимволы (#, @, $ и т. п.)',
   2,
   true
 );
 
-pristine.addValidator(
-  form.querySelector('.text__hashtags'),
-  ()=> {
-    const re = /^#[A-Za-zА-Яа-яЁё0-9]{1,19}$/;
-    const testValid = re.test(fieldHashtag.value);
-    return testValid;
-  },
-  'хэш-тег начинается с символа # и не может содержать пробелы, спецсимволы (#, @, $ и т. п.)',
-  1,
-  true
-);
-
-form.addEventListener('submit', (evt)=>{
-  evt.preventDefault();
-  pristine.validate();
-});
-
-
 // Ф-я скрытия формы редактирования изображения:
-function hiddenEditFormImg(){
+function closeEditForm(){
   editFormImg.classList.add('hidden');
   document.querySelector('body').classList.remove('modal-open');
   imgUploadPreview.querySelector('img').value = '';
 }
 
 function onCloseClick(){
-  hiddenEditFormImg();
+  closeEditForm();
   closeEditFormImg.removeEventListener('click', onCloseClick);
 }
 
 closeEditFormImg.addEventListener('click', onCloseClick);
+
 // Ф-я открытия формы редактирования изображения:
 fieldUpload.addEventListener('change', ()=>{
   editFormImg.classList.remove('hidden');
@@ -65,7 +84,7 @@ fieldUpload.addEventListener('change', ()=>{
 
 function onEditFormImgKeydown(evt) {
   if(evt.keyCode === 27){
-    hiddenEditFormImg();
+    closeEditForm();
   }
 }
 
