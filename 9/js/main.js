423ed7948fa4a8b846d45c2052cdbac134b2a5b3
Keks <keks@htmlacademy.ru>
2022-07-16 02:16:38 +0300

    :heavy_check_mark: Сборка #9

---
+++ 9/js/main.js
@@ -0,0 +1,8 @@
+import './user-form.js';
+import './render.js';
+import './controls.js';
+import { mocks } from './mock-data.js';
+import {renderPictures} from './render.js';
+renderPictures(mocks);
+// window.console.log(renderPictureCards(mocks,getPictureCardTemplate));
+
