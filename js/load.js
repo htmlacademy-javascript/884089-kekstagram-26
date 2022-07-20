cbe4a8b6683af9508aa94bf78c463f6d4c5c96d9
AlexDrags <vera2052@mail.ru>
2022-07-20 23:34:47 +0300

    Добавил ф-ю если при загрузке данных с сервера произошла ошибка

--- js/load.js
+++ js/load.js
@@ -1,9 +1,18 @@
+const errorLoadTemplate = document.querySelector('#js-error--load').content.querySelector('.error');
+const errorLoad = errorLoadTemplate.cloneNode(true);
+
+function erroView(){
+  document.body.append(errorLoad);
+}
+
 const load = function(fooRender){
   fetch('https://26.javascript.pages.academy/kekstagram/data')
     .then((response) => response.json())
     .then((date) => {
       fooRender(date);
+    })
+    .catch(() => {
+      erroView();
     });
 };
 export {load};
-
